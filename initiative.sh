58b3f76b0e626e2b31ad261415130c48975d5e07
Gleb Karpushkin <Luccifer@users.noreply.github.com>
2018-04-23 16:18:35 +0300

    Update initiative.sh

--- initiative.sh
+++ initiative.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
+wget https://rawgit.com/rsp/scripts/master/internalip
+chmod a+x internalip
+serverip = $(internalip)
+echo serverip
+
+echo "postfix postfix/mailname string $serverip" | debconf-set-selections
+echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
+
 echo Activating the Initiative protocols Heses
 
 sudo add-apt-repository -y ppa:linrunner/tlp
