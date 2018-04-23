097d8a4b66b42fa54f1d6b8cdf53104e53e2d075
Gleb Karpushkin <Luccifer@users.noreply.github.com>
2018-04-23 15:52:22 +0300

    Update initiative.sh

--- initiative.sh
+++ initiative.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo Activating the Initiative protocols Heses
 
-sudo add-apt-repository ppa:apt-fast/stable
-sudo add-apt-repository ppa:linrunner/tlp
+sudo add-apt-repository -y ppa:apt-fast/stable
+sudo add-apt-repository -y ppa:linrunner/tlp
 sudo apt-get update
 sudo apt-get install -y apt-fast 
 sudo apt-fast install -y preload tlp tlp-rdw indicator-cpufreq 
