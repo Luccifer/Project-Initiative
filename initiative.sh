34745d546bbc18274ea626b7d7b0bd8612cedb9d
Gleb Karpushkin <Luccifer@users.noreply.github.com>
2018-04-24 14:52:33 +0300

    Update initiative.sh

--- initiative.sh
+++ initiative.sh
@@ -5,7 +5,8 @@ export DEBIAN_FRONTEND=noninteractive
 #----------------server ip getter-------------------#
 wget https://rawgit.com/rsp/scripts/master/internalip
 chmod a+x internalip
-serverip = "$(internalip)"
+serverip = $(internalip)
+echo $serverip
 
 #------supress userinteraction in mail, slapd--------#
 echo "postfix postfix/mailname string $serverip" | debconf-set-selections
@@ -16,6 +17,8 @@ debconf-set-selections <<< 'slapd/root_password_again 123'
 echo Activating the Initiative protocols Hades
 
 sudo add-apt-repository -y ppa:linrunner/tlp
+echo "deb https://packagecloud.io/grafana/stable/debian/ stretch main" | sudo tee -a /etc/apt/sources.list #grafana
+curl https://packagecloud.io/gpg.key | sudo apt-key add - #grafana
 sudo apt-get update
 /bin/bash -c "$(curl -sL https://git.io/vokNn)" #apt-fast install
 sudo apt-fast install -y preload tlp tlp-rdw indicator-cpufreq zsh  git-core
@@ -23,9 +26,11 @@ sudo tlp start
 
 echo Activating the Initiative protocols Gaia
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh #zsh install
+sudo apt-fast install -y grafana apt-transport-https slapd ldap-utils #grafana + ldap
+sudo service grafana-server start
+sudo update-rc.d grafana-server defaults
+sudo systemctl enable grafana-server.service
+grafana-cli plugins install raintank-worldping-app
 
-sudo apt-fast install -yslapd ldap-utils #install LDAP server
-
-
-chsh -s `which zsh`
+chsh -s `which zsh` #choose zsh as default shell
 sudo shutdown -r 0
